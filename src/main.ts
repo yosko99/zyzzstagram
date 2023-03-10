df02f1b335d6b1c193b4918dfddd494a74284edf
yosko99 <thedriftmaniaka1997@gmail.com>
2023-03-10 10:53:03 +0200

    Init

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { NestFactory } from '@nestjs/core';
+
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('zysstagram API')
+    .setDescription('Endpoints')
+    .setVersion('1.0')
+    .addTag('API')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
